33eea83ff08fd3f63037d22b6e060a358043549c
jstrickler <jstrickler@gmail.com>
2018-10-08 15:06:37 -0400

    end of chapter 3

---
+++ cmd_line_args.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import sys
+
+print(sys.argv)  #  cmdline, split on whitespace
+
+print(sys.argv[0])  # script name
+print(sys.argv[1])  # 1st parameter
+print(sys.argv[2])
---
+++ reading_the_keyboard.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+knight_name = input("What is your name?\n")
+knight_quest = input("What is your quest? ")
+knight_color = input("What.....is your favorite color? ")
+
+print(f"Welcome, {knight_name}")
